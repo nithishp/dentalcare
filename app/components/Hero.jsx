6f1f2b099aa70dc89c9a095755994f62d66959d4
nithishp <nithishj03@gmail.com>
2024-10-30 12:22:27 +0530

    added menu and hero-bg for small devices

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,47 +3,49 @@ import React from 'react'
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { CircleArrowDown, CircleArrowRight } from 'lucide-react';
+import { WordPullUp } from './ui/word-pull-up';
 const Hero = () => {
   return (
-    <div className="bg-[url('/hero-bg-image.png')] pl-3 section  w-full bg-cover h-[95vh] rounded-2xl flex flex-col items-start justify-center">
-    <div className="flex flex-col items-start">
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1 }}
-        className="text-lg font-medium text-neutral-900 lg:text-nowrap text-wrap max-w-[70vw] -mb-3 ml-3 "
-      >
-        Expert Dental care in namma Nagercoil !!!
-      </motion.p>
-      <motion.h1
-        initial={{ y: -100, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.8 }}
-        className="text-neutral-950 font-semibold text-[20vw] lg:text-[20vh] text-left max-w-[500px] leading-0"
-      >
-        Dental
-      </motion.h1>
-      <motion.h1
-        initial={{ y: -100, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.9, delay: 0.2 }}
-        className="text-neutral-950 font-semibold text-[20vw] lg:text-[20vh] text-left max-w-[500px] leading-none"
-      >
-        Care
-      </motion.h1>
-      <motion.button
-        whileHover={{ backgroundColor:'transparent',color:'#0a0a0a', outline: '5px solid #0a0a0a'}}
-        className="px-6 py-4 bg-neutral-950 rounded-[50px] mt-10 font-semibold"
-      >
-        Get Started
-      </motion.button>
+    <div className=" bg-[url('/small-hero-bg.png')] lg:bg-[url('/hero-bg-image.png')] pl-3 section  w-full bg-cover h-[95vh] rounded-2xl flex flex-col items-start justify-center">
+      <div className="flex flex-col items-start">
+        <WordPullUp
+          className="text-lg font-medium text-neutral-900 lg:text-nowrap text-wrap max-w-[70vw] -mb-3 ml-3 "
+          words="Expert Dental care in namma Nagercoil !!!"
+        />
+
+        <motion.h1
+          initial={{ y: -100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="text-neutral-950 font-semibold text-[20vw] lg:text-[20vh] text-left max-w-[500px] leading-0"
+        >
+          Dental
+        </motion.h1>
+        <motion.h1
+          initial={{ y: -100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9, delay: 0.2 }}
+          className="text-neutral-950 font-semibold text-[20vw] lg:text-[20vh] text-left max-w-[500px] leading-none"
+        >
+          Care
+        </motion.h1>
+        <motion.button
+          whileHover={{
+            backgroundColor: "transparent",
+            color: "#0a0a0a",
+            outline: "5px solid #0a0a0a",
+          }}
+          className="px-6 py-4 bg-neutral-950 rounded-[50px] mt-10 font-semibold"
+        >
+          Get Started
+        </motion.button>
+      </div>
+      <div className="absolute flex gap-6 justify-center items-center right-0 lg:right-5 rotate-90">
+        <p className="text-neutral-950 ">Scroll to explore</p>
+        <CircleArrowRight className="text-neutral-950" />
+      </div>
     </div>
-    <div className='absolute flex gap-6 justify-center items-center right-0 lg:right-5 rotate-90'>
-        <p className='text-neutral-950 '>Scroll to explore</p>
-        <CircleArrowRight className='text-neutral-950' />
-    </div>
-  </div>
-  )
+  );
 }
 
 export default Hero
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import {Poppins} from "next/font/google";
 import "./globals.css";
+import Nav from "./components/CornerNav";
 
 
 const poppins = Poppins({subsets:['latin'],weight:['100','200','300','400','500','600','700','800']})
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${poppins.className}  antialiased`}
       >
+        <Nav/>
         {children}
       </body>
     </html>
